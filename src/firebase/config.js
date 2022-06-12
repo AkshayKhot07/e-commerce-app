82ea105fc618e2c9a409853d89a06bb0669e244f
AkshayKhot07 <akshaykhot743@gmail.com>
2022-06-12 13:11:24 +0530

    App and Firebase initialization

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0OVK4c6CAoaGkrjUsxy7JLTp4Ib-fWtU",
+  authDomain: "e-commerce-app-b7b4c.firebaseapp.com",
+  projectId: "e-commerce-app-b7b4c",
+  storageBucket: "e-commerce-app-b7b4c.appspot.com",
+  messagingSenderId: "987142648230",
+  appId: "1:987142648230:web:844986a9601cd1502d9f67",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth };
