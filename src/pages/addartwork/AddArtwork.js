aede3e8d7fbe9168203163c33e2d42d69b790631
AkshayKhot07 <akshaykhot743@gmail.com>
2022-06-14 23:26:52 +0530

    storing data on firestore functionality implemented

--- src/pages/addartwork/AddArtwork.js
+++ src/pages/addartwork/AddArtwork.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { projectStorage } from "../../firebase/config";
+import { useFirestore } from "../../hooks/useFirestore";
 
 import "./AddArtwork.css";
 
@@ -6,17 +8,60 @@ export default function AddArtwork() {
   const [artworkTitle, setArtworkTitle] = useState("");
   const [artworkPrice, setArtworkPrice] = useState("");
   const [artworkImage, setArtworkImage] = useState(null);
+  const [artworkImageError, setArtworkImageError] = useState(null);
+  const { addDocument, deleteDocument, response } = useFirestore("artworks");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(artworkTitle, artworkPrice);
+
+    //upload artwork image to firebase storage
+    const uploadPath = `artworks/${artworkImage.name}`;
+    const img = await projectStorage.ref(uploadPath).put(artworkImage);
+    const imgUrl = await img.ref.getDownloadURL();
+
+    console.log(artworkTitle, artworkPrice, imgUrl);
+
+    //add artwork to firestore
+    await addDocument({ artworkTitle, artworkPrice, photoURL: imgUrl });
+
+    const artWorkFile = document.querySelector(".artwork-file");
+    artWorkFile.value = null;
   };
 
   const handleFileChange = (e) => {
     setArtworkImage(null);
     let selected = e.target.files[0];
     console.log(selected);
+
+    if (!selected) {
+      setArtworkImageError("Please select a file");
+      return;
+    }
+
+    if (!selected.type.includes("image")) {
+      setArtworkImageError("Selected file must be an image");
+      return;
+    }
+
+    // if (selected.size > 100000) {
+    //   setArtworkImageError("Image file must be less than 100kb");
+    //   return;
+    // }
+
+    setArtworkImageError(null);
+    setArtworkImage(selected);
+
+    console.log("art work updated");
   };
+
+  // reset the form fields
+  useEffect(() => {
+    if (response.success) {
+      setArtworkTitle("");
+      setArtworkPrice("");
+      setArtworkImage(null);
+    }
+  }, [response.success]);
 
   return (
     <div className="add-artwork">
@@ -31,6 +76,7 @@ export default function AddArtwork() {
             value={artworkTitle}
           />
         </label>
+
         <label>
           <span>Art Work Price:</span>
           <input
@@ -41,6 +87,7 @@ export default function AddArtwork() {
             value={artworkPrice}
           />
         </label>
+
         <label>
           <span>Art Work Image:</span>
           <input
@@ -49,8 +96,12 @@ export default function AddArtwork() {
             className="artwork-file"
             onChange={handleFileChange}
           />
+          {artworkImageError && (
+            <div className="error">{artworkImageError}</div>
+          )}
         </label>
-        <button>Submit</button>
+        {response.isPending && <button disabled>Loading</button>}
+        {!response.isPending && <button>Submit</button>}
       </form>
     </div>
   );
